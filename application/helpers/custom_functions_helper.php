afc8c7eb96e116d8ed0eac915ebe615c77411936
jpmarquina <marquina.jp016@gmail.com>
2021-09-02 17:46:21 +0800

    Update custom_functions_helper.php

--- application/helpers/custom_functions_helper.php
+++ application/helpers/custom_functions_helper.php
@@ -6,7 +6,7 @@ if ( ! function_exists( 'get_open_weather' ) ) {
 	
 	function get_open_weather($city_name="Tokyo") {
 		
-		$apikey				= "14284582f163641eb62b4415ddf657b8";
+		$apikey				= "YOUR_API_KEY";
         $country_code 		= "JP";
 		$curlurl			= "api.openweathermap.org/data/2.5/weather?q={$city_name},{$country_code}&appid={$apikey}";
 		
@@ -27,8 +27,8 @@ if ( ! function_exists( 'get_foursquare_venue' ) ) {
 	function get_foursquare_venue($city_name="Tokyo") {
 		
 		$city_name			= ucwords($city_name);
-		$fs_client_id		= "XDSMJ1CQSYYA4LBVK4SNGE0UU0EU5I2IEXDR2TTO4T12DDOI";
-		$fs_client_secret	= "4N5DWC1FUDL0KQ5WLYCR4SZZKEIS4NZCPNYYMMOY0EM4J2O4";
+		$fs_client_id			= "YOUR_CLIENT_ID";
+		$fs_client_secret		= "YOUR_CLIENT_SECRET";
 		$curlurl 			= "https://api.foursquare.com/v2/venues/explore?near={$city_name},JP&client_id={$fs_client_id}&client_secret={$fs_client_secret}&v=20180323&limit=1&ll=40.7243,-74.0018&query=Tourist+Attractions";
 		 /* Init cURL resource */
         $ch = curl_init($curlurl);
